4922378c3cdc435f5394efb98b60e0492d84d63c
Pratibha-AJ <61814886+Pratibha-AJ@users.noreply.github.com>
2020-06-01 10:27:58 +1000

    First Draft

---
+++ WebApiOauth2/Provider/OauthProvider.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace WebApiOauth2.Provider
+{
+    public class OauthProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            // This method will be executed first to validate if the request has crendtials(UserName and Password)
+            //if the request does not contain username and 
+           //password the request will not proceded any further
+            context.Validated(); 
+        }
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+           
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType); //it will check the authenticate type
+            var user = "TestUser";
+            var pwd = "testpasword";
+
+            //Code to verify username and password are valid and Generate token
+
+            if (user != null)
+            {
+                //Store information againest the request
+                identity.AddClaim(new Claim("UserName", context.UserName));
+                identity.AddClaim(new Claim("LoggedOn", DateTime.Now.ToString()));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("Wrong Crendtials", "Provided username and password is incorrect");
+                context.Rejected();
+
+            }
+        }
+    }
+}
---
+++ WebApiOauth2/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Owin;
+
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApiOauth2.Provider;
+
+[assembly: OwinStartupAttribute(typeof(WebApiOauth2.Startup))]
+
+
+
+namespace WebApiOauth2
+{
+
+    public class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);// to enable cross-site HTTP requests  
+                                              //For security reasons, browsers restrict cross-origin HTTP requests 
+            var OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60),//token expiration time
+                Provider = new OauthProvider()
+            };
+
+            app.UseOAuthBearerTokens(OAuthOptions);
+            app.UseOAuthAuthorizationServer(OAuthOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);//register the request
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+    }
+
+}
